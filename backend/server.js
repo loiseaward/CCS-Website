f70dff53db9bf9ad422f12659b180dcabb3cdd13
Michael Salvador <108254009+mavsalvador@users.noreply.github.com>
2026-07-08 02:45:35 +0000

    set up barebones backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
 //main backend file
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.port || 3000; //can change later if needed
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Backend server is operational");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on https://localhost:${PORT}`);
+});
