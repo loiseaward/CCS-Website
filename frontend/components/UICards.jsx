973030cf9e6470079b785587d7c6708a3136506b
loiseaward <loise.award@gmail.com>
2026-07-09 08:35:49 -0400

    minor UI changes

--- frontend/components/UICards.jsx
+++ frontend/components/UICards.jsx
@@ -26,9 +26,9 @@ export function FAQAccordian() {
           aria-controls={`${id}-panel1-content`}
           id={`${id}-panel1-header`}
         >
-          <Typography component="span">Accordion 1</Typography>
+          <Typography sx={{fontFamily: 'Noto Serif, serif',color: '#7c3225',}} component="span">Accordion 1</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails sx={{fontFamily: 'Noto Serif, serif', color: '#64291f',}}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
         </AccordionDetails>
@@ -40,9 +40,9 @@ export function FAQAccordian() {
           aria-controls={`${id}-panel2-content`}
           id={`${id}-panel2-header`}
         >
-          <Typography component="span">Accordion 2</Typography>
+          <Typography sx={{fontFamily: 'Noto Serif, serif',color: '#7c3225',}} component="span">Accordion 2</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+        <AccordionDetails sx={{fontFamily: 'Noto Serif, serif', color: '#64291f',}}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
         </AccordionDetails>
@@ -54,9 +54,9 @@ export function FAQAccordian() {
           aria-controls={`${id}-panel2-content`}
           id={`${id}-panel2-header`}
         >
-          <Typography component="span">Accordion 2</Typography>
+          <Typography sx={{fontFamily: 'Noto Serif, serif',color: '#7c3225',}} component="span">Accordion 2</Typography>
         </AccordionSummary>
-        <AccordionDetails>
+          <AccordionDetails sx={{fontFamily: 'Noto Serif, serif', color: '#64291f',}}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           malesuada lacus ex, sit amet blandit leo lobortis eget.
         </AccordionDetails>
